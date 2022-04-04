62d52979057d9e0b67008b11c6172045a337ab0f
Your Name <dallongunn@gmail.com>
2022-04-03 23:18:06 -0600

    initial commit

---
+++ creative/front-end/src/App.vue
@@ -0,0 +1,186 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <!-- <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    /> -->
+
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+      <h1>Storywriter</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/test">Test</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="content-wrapper">
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      themeOverrides: {
+        caretColor: "var(--accent-color)",
+        boderColor: "var(--accent-color)",
+        borderFocus: "var(--accent-color)",
+        borderHover: "1px solid var(--accent-color)",
+        borderFocus: "1px solid var(--accent-color)",
+        // colorFocus: "rgb(189, 107, 0)"
+        boxShadowFocus: "0 0 8px 0 var(--accent-color)",
+        border: "0px solid #000000",
+      },
+      test: "test",
+    };
+  },
+};
+// export default defineComponent({});
+</script>
+
+<style>
+@import "@/assets/base.css";
+
+.wrapper nav {
+  background-color: var(--color-border);
+  padding: 5px;
+  border-radius: 10px;
+}
+
+.wrapper h1 {
+  color: var(--color-heading);
+  font-weight: 700;
+}
+
+.content-wrapper {
+  margin-left: auto;
+  margin-right: auto;
+  /* margin: 0px !important; */
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem;
+  max-width: 50rem;
+}
+
+.content {
+  width: 100%;
+}
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 2rem;
+}
+
+p {
+  margin-bottom: 0.3rem;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: var(--link-color);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    /* text-decoration: underline; */
+    background-color: var(--link-hover-color);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+/* @media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+} */
+</style>
---
+++ creative/front-end/src/views/TestView.vue
@@ -0,0 +1,236 @@
+<template>
+  <h2>Once Upon a Time</h2>
+  <h4>Part 1 ~ Jan 10, 2002</h4>
+
+  <section>
+    <p>
+      It was a dark and stormy night. People were doing stuff It was a lot of
+      fun and they had a really good time. The end It was a dark and stormy
+      night. People were doing stuff It was a lot of fun and they had a really
+      good time. The endIt was a dark and stormy night. People were doing stuff
+      It was a lot of fun and they had a really good time. The endIt was a dark
+      and stormy night. People were doing stuff It was a lot of fun and they had
+      a really good time. The endIt was a dark and stormy night. People were
+      doing stuff It was a lot of fun and they had a really good time. The endIt
+      was a dark and stormy night. People were doing stuff It was a lot of fun
+      and they had a really good time. The endIt was a dark and stormy night.
+      People were doing stuff It was a lot of fun and they had a really good
+      time. The endIt was a dark and stormy night. People were doing stuff It
+      was a lot of fun and they had a really good time. The end
+    </p>
+    <p>
+      It was a dark and stormy night. People were doing stuff It was a lot of
+      fun and they had a really good time. The end It was a dark and stormy
+      night. People were doing stuff It was a lot of fun and they had a really
+      good time. The endIt was a dark and stormy night. People were doing stuff
+      It was a lot of fun and they had a really good time. The endIt was a dark
+      and stormy night. People were doing stuff It was a lot of fun and they had
+      a really good time. The endIt was a dark and stormy night. People were
+      doing stuff It was a lot of fun and they had a really good time. The endIt
+      was a dark and stormy night. People were doing stuff It was a lot of fun
+      and they had a really good time. The endIt was a dark and stormy night.
+      People were doing stuff It was a lot of fun and they had a really good
+      time. The endIt was a dark and stormy night. People were doing stuff It
+      was a lot of fun and they had a really good time. The end
+    </p>
+  </section>
+  <div class="options">
+    <p>
+      <span class="upvote-icon">👍</span> 123
+      <span class="downvote-icon">👎</span> 2
+    </p>
+
+    <n-button ghost color="rgb(189, 107, 0)">View Comments</n-button>
+    <n-button ghost color="rgb(189, 107, 0)">Add to Story</n-button>
+  </div>
+  <div class="comment-section">
+    <h3>Comments</h3>
+    <div class="new-comment">
+      <div class="comment-input">
+        <n-input
+          size="small"
+          v-model:value="value"
+          type="text"
+          placeholder="Name"
+          :theme="darkTheme"
+          :theme-overrides="themeOverrides"
+        />
+        <!-- <p></p> -->
+        <!-- <br /> -->
+        <n-input
+          type="textarea"
+          size="small"
+          placeholder="Write a Comment"
+          :autosize="{
+            minRows: 3,
+          }"
+          :theme="darkTheme"
+          :theme-overrides="themeOverrides"
+        />
+        <n-button ghost color="rgb(189, 107, 0)">Submit</n-button>
+      </div>
+    </div>
+
+    <div class="comment">
+      <div class="votes">
+        <span class="upvote-icon">👍</span>
+        <p>123</p>
+        <span class="downvote-icon">👎</span>
+      </div>
+      <div class="comment-content">
+        <h3>Bobbery</h3>
+        <p>Good Story</p>
+      </div>
+    </div>
+  </div>
+  <h4>Part 2 ~ Jan 12, 2022</h4>
+  <section>
+    <p>
+      It was a dark and stormy night. People were doing stuff It was a lot of
+      fun and they had a really good time. The end It was a dark and stormy
+      night. People were doing stuff It was a lot of fun and they had a really
+      good time. The endIt was a dark and stormy night. People were doing stuff
+      It was a lot of fun and they had a really good time. The endIt was a dark
+      and stormy night. People were doing stuff It was a lot of fun and they had
+      a really good time. The endIt was a dark and stormy night. People were
+      doing stuff It was a lot of fun and they had a really good time. The endIt
+      was a dark and stormy night. People were doing stuff It was a lot of fun
+      and they had a really good time. The endIt was a dark and stormy night.
+      People were doing stuff It was a lot of fun and they had a really good
+      time. The endIt was a dark and stormy night. People were doing stuff It
+      was a lot of fun and they had a really good time. The end
+    </p>
+    <p>
+      It was a dark and stormy night. People were doing stuff It was a lot of
+      fun and they had a really good time. The end It was a dark and stormy
+      night. People were doing stuff It was a lot of fun and they had a really
+      good time. The endIt was a dark and stormy night. People were doing stuff
+      It was a lot of fun and they had a really good time. The endIt was a dark
+      and stormy night. People were doing stuff It was a lot of fun and they had
+      a really good time. The endIt was a dark and stormy night. People were
+      doing stuff It was a lot of fun and they had a really good time. The endIt
+      was a dark and stormy night. People were doing stuff It was a lot of fun
+      and they had a really good time. The endIt was a dark and stormy night.
+      People were doing stuff It was a lot of fun and they had a really good
+      time. The endIt was a dark and stormy night. People were doing stuff It
+      was a lot of fun and they had a really good time. The end
+    </p>
+  </section>
+</template>
+
+<script>
+// console.log(this.$root.test);
+import { defineComponent } from "vue";
+import { NButton } from "naive-ui";
+import { NInput } from "naive-ui";
+import { darkTheme } from "naive-ui";
+
+export default defineComponent({
+  setup() {
+    return {
+      darkTheme,
+    };
+  },
+  data() {
+    return {
+      themeOverrides: {
+        caretColor: "var(--accent-color)",
+        boderColor: "var(--accent-color)",
+        borderFocus: "var(--accent-color)",
+        borderHover: "1px solid var(--accent-color)",
+        borderFocus: "1px solid var(--accent-color)",
+        // colorFocus: "rgb(189, 107, 0)"
+        boxShadowFocus: "0 0 8px 0 var(--accent-color)",
+        border: "0px solid #000000",
+      },
+    };
+  },
+
+  components: {
+    NButton,
+    NInput,
+  },
+});
+</script>
+
+<style scoped>
+.comment-input {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  /* justify-content: space-around; */
+}
+.new-comment {
+  display: flex;
+  /* align-items: center; */
+  gap: 8px;
+  /* width: 80rem; */
+  /* width: 100%; */
+  /* padding: 0.8rem; */
+  padding: 0.6rem;
+}
+.options {
+  padding: 5px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+
+.comment {
+  /* background-color: var(--color-background-secondary); */
+  display: flex;
+  /* margin-left: 4rem; */
+}
+.comment-content {
+  padding: 0.8rem;
+  margin: 0.5rem;
+  border-radius: 5px;
+  border-color: var(--color-border-light);
+  border-style: solid;
+  width: 100%;
+}
+
+.comment-section h3 {
+  color: var(--color-heading);
+  font-weight: 500;
+}
+
+.comment-content p {
+  color: var(--color-text);
+}
+
+.votes {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.comment h3 {
+  color: var(--color-heading);
+  /* font-weight: 500; */
+}
+
+h2 {
+  color: var(--color-heading);
+  font-weight: 500;
+}
+
+h4 {
+  font-size: 1rem;
+  /* font-weight: 500; */
+  margin-bottom: 0.4rem;
+  color: var(--color-text);
+}
+
+section {
+  background-color: var(--color-background-secondary);
+  padding: 0.8rem;
+  border-radius: 5px;
+}
+
+p {
+  color: var(--color-story-text);
+  line-height: 1.8rem;
+}
+</style>
